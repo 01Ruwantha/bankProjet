c5bfdd3e0c879f3b7cf2c9f93f31cab54a8f7dab
01Ruwantha <2000ruwantha@gmail.com>
2025-01-26 15:57:33 +0530

    create InterfaceAccount

---
+++ src/InterfaceAccount.java
@@ -0,0 +1,6 @@
+public interface InterfaceAccount {
+    boolean cashWithdraw(int withdrawAmount);
+    boolean  cashDeposit(Double dipositeAmount);
+     int accountNumberGenerate();
+     int accountNumberGenerate(String nullValue);
+}
