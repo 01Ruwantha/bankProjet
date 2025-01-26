563f16085247644706215c1357aae93d27f4b382
01Ruwantha <2000ruwantha@gmail.com>
2025-01-26 15:51:24 +0530

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Audio.mainWindowMassage();
+        final LaunchPages LaunchMainWindow=new LaunchPages();
+        LaunchMainWindow.MainWindow();
     }
 }
